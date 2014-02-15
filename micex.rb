d413732126af7d22b1e78f286aae20969ab05ef5
Ivan Povalyukhin <ivanpoval@gmail.com>
2014-02-14 22:26:38 -0800

    added basisc ticker prices

--- micex.rb
+++ micex.rb
@@ -3,31 +3,47 @@
 
 # Help
 # http://www.finam.ru/analysis/leaders/
+#
+# Start
+# WHEN=current|last_day ruby ./$0.rb
+#
 
 require 'json'
 require 'open-uri'
+require_relative 'notifyme'
 
-TGK1 = 'profile047CE'
-GZPR = 'profile041CA'
+@url = {
+  'yesterday' => 'http://www.finam.ru/service.asp?name=leaders&action=grid&market=1&pitch=1&sorting.name=price-pchange&sorting.dir=desc&count=-1',
+  'today' => 'http://www.finam.ru/service.asp?name=leaders&action=grid&market=1&pitch=5&sorting.name=price-pchange&sorting.dir=desc&count=-1&price-pchange.min=null&price-pchange.max=null',
+}.fetch ENV['WHEN']
+@page = open(@url).read
 
-def micex_run
-  @url = 'http://www.finam.ru/service.asp?name=leaders&action=grid&market=1&pitch=5&sorting.name=price-pchange&sorting.dir=desc&count=-1&price-pchange.min=null&price-pchange.max=null'
+def micex_run(ticker)
+  # @page.encode! Encoding::UTF_8, invalid: :replace, replace: ''
 
-  page = open(@url).read
-  # page.encode! Encoding::UTF_8, invalid: :replace, replace: ''
-
-  str = page.match(/#{TGK1}.+?(?=<\/tr)/).to_s
-
-  price_change_match = str.match(/<td class='price-pchange.*?'>([+-]?.+?)<\/td>/)
+  page = @page.match(/#{ticker[:profile_id]}.+?(?=<\/tr)/).to_s
+  price_change_match = page.match(/<td class='price-pchange.*?'>([+-]?.+?)<\/td>/)
 
   if price_change_match
     price_change = price_change_match[1]
     price_change_int = price_change.to_i
 
-    if true || price_change_int.abs >= 1
-      printf "MICEX: %s / %d / PRICE CHANGE: \e[0;31m%s\e[0m / %s\n" % ['TGK1', price_change_int, price_change, 'http://www.finam.ru/analysis/profile047CE']
+    if price_change_int.abs >= ticker[:percentage]
+      printf "MICEX: %s / %d / PRICE CHANGE: \e[0;31m%s\e[0m / %s\n" % [ticker[:name], price_change_int, price_change, "http://www.finam.ru/analysis/#{ticker[:profile_id]}"]
+      notifyme
     end
+  else
+    puts 'DID NOT FIND Price Match for %s' % ticker[:name]
   end
 end
+
+TICKERS = [
+  { name: 'TGK1', profile_id: 'profile047CE', percentage: 0 },
+  { name: 'GZPR', profile_id: 'profile041CA', percentage: 0 },
+]
+
+TICKERS.each do |ticker|
+  micex_run ticker
+end; nil
 
 __END__
