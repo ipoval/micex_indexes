dc7503da6cc69d059edb16ea9129df86d79fe885
Ivan Povalyukhin <ivanpoval@gmail.com>
2014-02-15 15:13:00 -0800

    updated the list of stock tickers

--- micex.rb
+++ micex.rb
@@ -11,12 +11,12 @@
 require 'json'
 require 'open-uri'
 require_relative 'notifyme'
+require_relative './tickers'
 
 @url = {
   'yesterday' => 'http://www.finam.ru/service.asp?name=leaders&action=grid&market=1&pitch=1&sorting.name=price-pchange&sorting.dir=desc&count=-1',
   'today' => 'http://www.finam.ru/service.asp?name=leaders&action=grid&market=1&pitch=5&sorting.name=price-pchange&sorting.dir=desc&count=-1&price-pchange.min=null&price-pchange.max=null',
 }.fetch ENV['WHEN']
-@page = open(@url).read
 
 def micex_run(ticker)
   # @page.encode! Encoding::UTF_8, invalid: :replace, replace: ''
@@ -29,21 +29,30 @@ def micex_run(ticker)
     price_change_int = price_change.to_i
 
     if price_change_int.abs >= ticker[:percentage]
-      printf "MICEX: %s / %d / PRICE CHANGE: \e[0;31m%s\e[0m / %s\n" % [ticker[:name], price_change_int, price_change, "http://www.finam.ru/analysis/#{ticker[:profile_id]}"]
-      notifyme
+      html = sprintf "MICEX: %s / %d / PRICE CHANGE: \e[0;31m%s\e[0m / %s\n" % [ticker[:name], price_change_int, price_change, ticker[:url]]
+
+      puts ("\e[0;31m%40s\e[0m" % ["ALARM #{Time.now.to_s}"]).tr(" ", "=")
+      puts html
+      puts ("\e[0;31m%40s\e[0m" % ["ALARM #{Time.now.to_s}"]).tr(" ", "=")
+
+      notifyme html
+    else
+      print "%s: %-20s: %s %60s\n" % [Time.now.utc.ctime, ticker[:name], price_change, ticker[:url]]
     end
   else
     puts 'DID NOT FIND Price Match for %s' % ticker[:name]
   end
 end
 
-TICKERS = [
-  { name: 'TGK1', profile_id: 'profile047CE', percentage: 0 },
-  { name: 'GZPR', profile_id: 'profile041CA', percentage: 0 },
-]
+loop do
+  @page = open(@url).read
 
-TICKERS.each do |ticker|
-  micex_run ticker
-end; nil
+  TICKERS.each { |ticker| micex_run ticker }
+
+  print ("%60s\n" % "").tr(' ', '-')
+
+  sleep 15
+
+end
 
 __END__
